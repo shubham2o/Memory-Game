77a427866725f2992124a0f14b23f39c37ac7e94
shubham2o <shubhamsharma.ha@gmail.com>
2022-08-21 01:49:00 +0530

    HTML DOM getAttribute() added and Doubts completed

--- Doubts/Doubts.js
+++ Doubts/Doubts.js
@@ -4,8 +4,21 @@ function myFunction() {
     document.getElementById("myH1").setAttribute("class", "democlass");
 }
 
+
 // HTML DOM Element getAttribute()
 // EXAMPLE 1
 const element = document.getElementById('myH2');
 let text = element.getAttribute('class');
 document.getElementById('demo').innerHTML = text;
+
+// EXAMPLE 2
+const myAnchor = document.getElementById('myAnchor');
+let text2 = myAnchor.getAttribute('target');
+document.getElementById('demo2').innerHTML = text2;
+
+// EXAMPLE 3
+function myFunction3() {
+    const myButton = document.getElementById('myButton');
+    const text3 = myButton.getAttribute('onclick');
+    document.getElementById('demo3').innerHTML = text3;
+}
