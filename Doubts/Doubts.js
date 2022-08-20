b99677398eb778029cc04bfb19dc838830a101b8
shubham2o <shubhamsharma.ha@gmail.com>
2022-08-21 00:12:47 +0530

    HTML DOM Element getAttribute() example1 added

--- Doubts/Doubts.js
+++ Doubts/Doubts.js
@@ -3,3 +3,9 @@
 function myFunction() {
     document.getElementById("myH1").setAttribute("class", "democlass");
 }
+
+// HTML DOM Element getAttribute()
+// EXAMPLE 1
+const element = document.getElementById('myH2');
+let text = element.getAttribute('class');
+document.getElementById('demo').innerHTML = text;
